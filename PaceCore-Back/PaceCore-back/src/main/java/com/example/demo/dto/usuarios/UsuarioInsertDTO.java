d8e43778351dddc6878dd442e093fd853a2958e0
MariaAraceliPacheco <maricelipachecoarjona18@gmail.com>
2026-03-16 10:14:24 +0100

    validacion de DTO UsuarioInsertDTO

--- PaceCore-Back/PaceCore-back/src/main/java/com/example/demo/dto/usuarios/UsuarioInsertDTO.java
+++ PaceCore-Back/PaceCore-back/src/main/java/com/example/demo/dto/usuarios/UsuarioInsertDTO.java
@@ -1,33 +1,58 @@
 package com.example.demo.dto.usuarios;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "Datos necesarios para registrar un nuevo usuario")
 public class UsuarioInsertDTO {
 
+	@NotBlank
 	@Schema(description = "Nombre completo del usuario", example = "Juan Pérez")
 	private String nombre;
+
+	@Email
+	@NotBlank
 	@Schema(description = "Correo electrónico único", example = "juan.perez@example.com")
 	private String email;
+
+	@NotBlank
+	@Size(min = 6, max = 64)
 	@Schema(description = "Contraseña de acceso", example = "password123")
 	private String password;
 
+	@Size(max = 255)
 	@Schema(description = "Breve descripción o biografía", example = "Entusiasta del running")
 	private String descripcion;
+
+	@DecimalMin("0")
+	@DecimalMax("400")
 	@Schema(description = "Peso en kilogramos", example = "75.5")
 	private BigDecimal peso;
-	@Schema(description = "Altura en metros", example = "1.80")
+
+	// como este es de tipo BigDecimal, se usa la notacion @DecimalMin y @DecimalMax
+	@DecimalMin("0")
+	@DecimalMax("350")
+	@Schema(description = "Altura en centimetros", example = "180")
 	private BigDecimal altura;
 	@Schema(description = "Edad del usuario", example = "30")
+
+	// este es un tipo numero Entero, por lo que se usa la anotacion @Min y @Max
+	@Min(0)
+	@Max(150)
+	@NotNull
 	private Integer edad;
 
-	public UsuarioInsertDTO(String nombre, String email, String password, String descripcion,
-			Integer edad, BigDecimal peso, BigDecimal altura) {
+	public UsuarioInsertDTO(String nombre, String email, String password, String descripcion, Integer edad,
+			BigDecimal peso, BigDecimal altura) {
 		super();
 		this.nombre = nombre;
 		this.email = email;
